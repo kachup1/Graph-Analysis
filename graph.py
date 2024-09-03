bdb0e96e0e77d56eaba7165419825a86d4a1e8a7
kachup1 <emailkarlahere@gmail.com>
2024-09-02 18:38:54 -0700

    added nodes edges and print statements

--- graph.py
+++ graph.py
@@ -1,2 +1,15 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+
+#create a graph with no nodes and no edges
+G = nx.Graph()
+
+#add one note
+G.add_node(2)
+
+#adding edges
+G.add_edge(1,2)
+
+#print graphs info
+nx.draw(G, with_labels=True)
+plt.show()
