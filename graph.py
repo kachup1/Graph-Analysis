8f2fa60bc45bd88c057cc07c0fccdaa005b59987
kachup1 <emailkarlahere@gmail.com>
2024-09-07 20:36:10 -0700

    added error handling

--- graph.py
+++ graph.py
@@ -5,7 +5,6 @@ import math
 import sys #reads command line arguments
 
 '''
-
 Function that reads the command lines from terminal
 
 python ./graph.py --input graph_file.gml --create_random_graph 
@@ -21,7 +20,6 @@ python ./graph.py --input graph_file.gml --create_random_graph
 '''
 
 def CommandLineArgs(argv):
-
     inputFile = ''
     create_random_graph = False
     nodes = 0
@@ -31,38 +29,47 @@ def CommandLineArgs(argv):
     BFS = ''
     outputFile = ''
 
-
     i = 1
     while i < len(argv):
         if argv[i] == '--input':
             inputFile = argv[i+1]
-            i += 2 #moves to next set of arguments
+            if not inputFile.endswith('.gml'):
+                print("Error: Input file must be of type .gml")
+                sys.exit(1)
+            i += 2
         elif argv[i] == '--create_random_graph':
             create_random_graph = True
             i += 1
         elif argv[i] == '--nodes':
-            nodes = int(argv[i+1])
-            i += 2 
+            try:
+                nodes = int(argv[i+1])
+                i += 2
+            except ValueError:
+                print("Error: --nodes must be an integer.")
+                sys.exit(1)
         elif argv[i] == '--constant':
-            constant = float(argv[i+1])
-            probability = float((constant * math.log(nodes)) / nodes)
-            i += 2 
+            try:
+                constant = float(argv[i+1])
+                probability = (constant * math.log(nodes)) / nodes
+                i += 2
+            except ValueError:
+                print("Error: --constant must be a float.")
+                sys.exit(1)
         elif argv[i] == '--plot':
             plot = True
-            i += 1 
+            i += 1
         elif argv[i] == '--BFS':
             BFS = argv[i+1]
-            i += 2 
+            i += 2
         elif argv[i] == '--output':
             outputFile = argv[i+1]
-            i += 2 
+            i += 2
         else:
-            print("Error. Invalid input")
+            print(f"Error: Invalid argument {argv[i]}")
             sys.exit(1)
+    
     return [inputFile, create_random_graph, nodes, probability, plot, BFS, outputFile]
 
-
-    
 
 def main():
 
@@ -77,10 +84,7 @@ def main():
     outputFile = input[6]
 
     if inputFile:
-        if not inputFile.endswith('.gml'):
-            print("Error. Input file must be type .gml")
-        else:
-            G = nx.read_gml(inputFile) #loads graph from gml file
+        G = nx.read_gml(inputFile) #loads graph from gml file
     elif create_random_graph:
         if nodes <= 0:
             print("Error. Nodes must be greater than 0.")
@@ -88,7 +92,6 @@ def main():
         G = nx.erdos_renyi_graph(nodes, probability)# generates erdos
         G = nx.relabel_nodes(G, lambda x: str(x)) #relabels nodes to str
         
-
     if BFS:
         if BFS not in G:
             print("Error. Node {BFS} does not exist in the graph.")
@@ -107,8 +110,5 @@ def main():
 
     print(sys.argv[1:])
    
-
-
-
 if __name__ == "__main__":
     main()
